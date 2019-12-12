5d164565f5bc4eec2f8e91edfe06990b5645fddf
Marcin <marcin.lozu@gmail.com>
2019-12-12 21:43:02 +0100

    better design
    new fonts, repair layout

---
+++ jsstyle.js
@@ -0,0 +1,70 @@
+function hideElementPurpose() {
+	const x = document.getElementById('celZawodowy')
+
+	if (x.style.display === "none") {
+    	x.style.display = "block";
+  	} else {
+    	x.style.display = "none";
+  }
+}
+
+function hideElementSkill() {
+	const x = document.getElementById('skills')
+
+	if (x.style.display === "none") {
+    	x.style.display = "block";
+  	} else {
+    	x.style.display = "none";
+  }
+}
+
+function hideElementAtut() {
+	const x = document.getElementById('atuty')
+
+	if (x.style.display === "none") {
+    	x.style.display = "block";
+  	} else {
+    	x.style.display = "none";
+  }
+}
+
+function hideElementEdu() {
+	const x = document.getElementById('education')
+
+	if (x.style.display === "none") {
+    	x.style.display = "block";
+  	} else {
+    	x.style.display = "none";
+  }
+}
+
+function hideElementHobby() {
+	const x = document.getElementById('hobbies')
+
+	if (x.style.display === "none") {
+    	x.style.display = "block";
+  	} else {
+    	x.style.display = "none";
+  }
+}
+
+function hideElementExp() {
+	const x = document.getElementById('exp')
+
+	if (x.style.display === "none") {
+    	x.style.display = "block";
+  	} else {
+    	x.style.display = "none";
+  }
+}
+
+
+function hideElementOtherSkills() {
+	const x = document.getElementById('skills2')
+
+	if (x.style.display === "none") {
+    	x.style.display = "block";
+  	} else {
+    	x.style.display = "none";
+  }
+}
